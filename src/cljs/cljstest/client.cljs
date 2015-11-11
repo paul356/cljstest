1d925538e6cc830aa5a95153a2feca638dae3ffd
paul <panhao356@gmail.com>
2015-11-11 23:31:27 +0800

    Use goog graphics to draw some basic shape based on the parameters get from global array "anchors". "anchors" is generated by backend file server.clj.

--- src/cljs/cljstest/client.cljs
+++ src/cljs/cljstest/client.cljs
@@ -1,9 +1,17 @@
 (ns hello-clojurescript
   (:require [goog.events :as events]
-            [goog.dom :as dom]))
+            [goog.dom :as dom]
+            [goog.graphics :as graphics]))
            
-(defn render-radios [] (.log js/console (str (js->clj js/anchors))))
+(defn render-radios [graph] 
+ (let [fill (graphics/SolidFill. "red")
+       stroke (graphics/Stroke. 2 "green")]
+  (doseq [[x y] (js->clj js/anchors)] 
+   (.drawCircle graph x y 5 fill stroke))))
 
 (defn main []
- (render-radios))
+ (let [graph (graphics/createGraphics "100%" "100%" 1000 600)
+       div-canvas (dom/getElement "canvas")] 
+  (render-radios graph)
+  (.render graph div-canvas)))
 (set! (.-onload js/window) main)
