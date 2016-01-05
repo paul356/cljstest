847378b1f1d3561a2b14604b878a167071b97a25
hao pan <panhao356@gmail.com>
2016-01-05 15:20:22 +0800

    Update project.clj
    Update dependencies and plugins to updated versions

--- project.clj
+++ project.clj
@@ -1,25 +1,26 @@
 (defproject cljstest "0.1.0-SNAPSHOT"
-  :description "cljstest"
-  :url "http://127.0.0.1/cljstest"
-  :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.clojure/clojurescript "0.0-2156"]
-                 [ring/ring-defaults "0.1.2" :exclusions [javax.servlet/servlet-api]]
-                 [cc.qbits/jet "0.7.0-beta1"]
-                 [compojure "1.3.1"]
-                 [org.scream3r/jssc "2.8.0"]]
-  :plugins [[lein-cljsbuild "1.0.2"]
-            [lein-ring "0.8.10"]]
-  :source-paths ["src/clj"]
-  :cljsbuild { 
-    :builds [
-      { :source-paths ["src/cljs"]
-        :compiler {:output-to "resources/public/js/cljs.js"
-                   :output-dir "resources/public/js/out"
-                   :optimizations :advanced
-                   :pretty-print true
-                   :source-map "resources/public/js/cljs.js.map"}
-        :jar true } 
-    ]}
-  :main cljstest.server
-  :ring {:handler cljstest.server/main})
-
+            :description "cljstest"
+            :url "http://127.0.0.1/cljstest"
+            :dependencies [[org.clojure/clojure "1.7.0"]
+                           [org.clojure/clojurescript "1.7.170"]
+                           [ring/ring-defaults "0.1.5" :exclusions [javax.servlet/servlet-api]]
+                           [cc.qbits/jet "0.7.1"]
+                           [compojure "1.4.0"]]
+            :plugins [[lein-cljsbuild "1.1.2"]
+                      [lein-ring "0.9.7"]
+                      [lein-figwheel "0.5.0-2"]]
+            :source-paths ["src/clj"]
+            :cljsbuild {
+                        :builds [{:id "main-build"
+                                  :source-paths ["src/cljs"]
+                                  :figwheel true
+                                  :compiler {
+                                             :main "cljstest.client"
+                                             :asset-path "js/out"
+                                             :output-to "resources/public/js/cljs.js"
+                                             :output-dir "resources/public/js/out"
+                                            }
+                                 }]
+                       }
+            :main cljstest.server
+            :ring {:handler cljstest.server/main})
